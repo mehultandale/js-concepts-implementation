1eafcbd3144e9afff531aad5b84ff47f28242f92
Mehul <mehultandale@gmail.com>
2022-12-05 16:20:53 +0530

    > Another point about IIFEs

--- iifeExample.js
+++ iifeExample.js
@@ -3,6 +3,13 @@
 (function () {
 
 	console.log("\x1b[44m%s\x1b[0m", "Concept: Immediately Invoked Function Expression (IIFE)");
-	console.log("This is logged from a IIFE \n\n");
+	console.log("This is logged from a IIFE.");
+	let a = 1;
+	console.log("The value of variable a is ", a , ".");
 
 })();
+
+console.log("The value of variable a would be undefined here.");
+console.log("IIFEs, thus, prevent the pollution of global namespace. \n\n");
+// Uncommenting the below line would throw an error
+// console.log(a);
