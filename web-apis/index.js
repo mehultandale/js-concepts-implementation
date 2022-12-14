0c0eb924dea361eab9c55de2254c177ccb6d9674
Mehul <mehultandale@gmail.com>
2022-12-14 14:20:56 +0530

    > Adding gradient to canvas

--- web-apis/index.js
+++ web-apis/index.js
@@ -1,14 +1,26 @@
 
+
 /* Getting and configuring the canvas element */
 const canvas = document.getElementById("canvasDemo");
+canvas.height = 350;
+canvas.width = 350;
 const ctx = canvas.getContext("2d");
 
 /* Adding text to it */
 ctx.font = "12px serif";
+ctx.fillStyle = "blue";
 ctx.fillText("This text is on a canvas!", 100, 100);
 
-/* Draw a line under it */
+/* Drawing a line under it */
 ctx.beginPath();
 ctx.moveTo(100, 110);
 ctx.lineTo(220, 110);
 ctx.stroke();
+
+/* Adding a gradient to a rectangle */
+let gradient = ctx.createLinearGradient(0, 130, 700, 700);
+gradient.addColorStop(0, "yellow");
+gradient.addColorStop(0.2, "red");
+gradient.addColorStop(0.6, "blue");
+ctx.fillStyle = gradient;
+ctx.fillRect(0, 130, 700, 700);
