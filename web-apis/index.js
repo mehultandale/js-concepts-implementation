85695837eba72ebfdaab522cd8dfa34746f7f435
Mehul <mehultandale@gmail.com>
2022-12-14 13:59:57 +0530

    > Add a line under the text

--- web-apis/index.js
+++ web-apis/index.js
@@ -6,3 +6,9 @@ const ctx = canvas.getContext("2d");
 /* Adding text to it */
 ctx.font = "12px serif";
 ctx.fillText("This text is on a canvas!", 100, 100);
+
+/* Draw a line under it */
+ctx.beginPath();
+ctx.moveTo(100, 110);
+ctx.lineTo(220, 110);
+ctx.stroke();
