6f6505fc225d20a59c2f32a1b6858dcaf68cd7af
Mehul <mehultandale@gmail.com>
2022-12-15 16:12:20 +0530

    > Adding an example of Console Table

--- web-apis/index.js
+++ web-apis/index.js
@@ -7,3 +7,17 @@ import canvasDemo from "./canvasDemo.js";
 /* Battery status */
 batteryStatusDemo.logBatteryStatus();
 canvasDemo.insertCanvas();
+
+/* Console Table example */
+function Employee(employeeID, employeeFirstName, employeeLastName, employeeDepartment) {
+	this.employeeID = employeeID;
+	this.employeeFirstName = employeeFirstName;
+	this.employeeLastName = employeeLastName;
+	this.employeeDepartment = employeeDepartment;
+}
+
+const john = new Employee("TSL1", "John", "Doe", "Development");
+const tom = new Employee("TSL2", "Tom", "Test", "IT");
+const alice = new Employee("TSL3", "Alice", "Test", "Sales");
+
+console.table([john, tom, alice]);
